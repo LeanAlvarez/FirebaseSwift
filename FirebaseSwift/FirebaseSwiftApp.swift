eed94bd152087f5d3fde71e8de97e2e15a1570e1
leanalvarez <leanalvarez.dev@gmail.com>
2024-07-03 09:53:46 -0300

    Initial Commit

---
+++ FirebaseSwift/FirebaseSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftApp.swift
+//  FirebaseSwift
+//
+//  Created by Lean on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
